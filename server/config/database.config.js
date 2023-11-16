01d00852a8b469a54fc5072aec042a579218185e
Michal Zonis <michal2467300@gmail.com>
2023-11-16 13:50:44 +0200

    server template

---
+++ server/config/database.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    url: 'mongodb://localhost:27017/Tripplan'
+}
+
+// require('../models/users');
---
+++ server/routes/base.route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const GameRouter = require('./game.route')
+
+router.get('/', function (req, res) {
+    res.send('API works!!!');
+});
+
+
+router.use('/game', GameRouter);
+
+
+module.exports = router;
