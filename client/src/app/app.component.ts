a42f947f054f7944833040d02728028f2a431d56
Michal Zonis <michal2467300@gmail.com>
2023-11-16 12:18:42 +0200

    client proj with initial map

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, GoogleMapsModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tripplan';
+  constructor() {}
+  ngOnInit(): void {}
+  display: any; // Property to store latitude and longitude data from the map
+  center: google.maps.LatLngLiteral = {
+    // Initial center coordinates for the map
+    lat: 35.51679331043587,
+    lng: 139.35149289364826 
+  };
+  zoom = 3; // Initial zoom level for the map
+  move(event: google.maps.MapMouseEvent) {
+    // Method to handle map click event and update the display property
+    if (event.latLng != null) {
+      this.display = event.latLng.toJSON();
+    }
+  }
+}
