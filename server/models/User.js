e9407daa4b746b7bd7007f24c18b3fbfc56ef9ae
Michal Zonis <michal2467300@gmail.com>
2023-11-18 22:56:40 +0200

    initial user schema

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// TODO: this is a template for an object model. create others as needed and rename or delete this one.
+
+// const gameSchema = new mongoose.Schema({
+// });
+
+// const game = mongoose.model("game", gameSchema);
+
+// module.exports = game;
