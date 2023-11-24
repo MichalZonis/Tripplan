b1eee023d1c27c2578051b04b65a93e626d1768e
Smichmich <michael.alimi2@gmail.com>
2023-11-24 12:39:56 +0200

    Front: fixed trip card component to match new trip plan model

--- client/src/app/components/trip-plan-card/trip-plan-card.component.ts
+++ client/src/app/components/trip-plan-card/trip-plan-card.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TripPlan } from "../../models/TripPlan";
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-trip-plan-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './trip-plan-card.component.html',
   styleUrl: './trip-plan-card.component.scss'
 })
