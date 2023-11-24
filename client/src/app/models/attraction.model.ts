ca634cee56114975006b75da038eadde19c019ac
Smichmich <michael.alimi2@gmail.com>
2023-11-24 11:23:48 +0200

    Front: refactored attraction model

--- client/src/app/models/attraction.model.ts
+++ client/src/app/models/attraction.model.ts
@@ -1,9 +1,21 @@
 export class Attraction {
     constructor(
-        private id: string,
-        private name: string,
-        private date: Date,
-        private link: string,
-        private tripID: string,
+        public id: string,
+        public name: string,
+        public GeolocationCoordinates: {
+            lat: number,
+            lng: number
+        },
+        public isOptional: boolean,
+        public visitHours: {
+            startTime: string,
+            endTime: string
+        },
+        public visitDates: {
+            startDate: Date,
+            endDate: Date
+        },
+        public description: string,
+        public attractionPrice: number,
     ) {}
 }
