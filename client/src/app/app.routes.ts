fe33cac6822b38ad33a59acc97e939aa4cba2fcb
Smichmich <michael.alimi2@gmail.com>
2023-11-24 12:38:58 +0200

    Front: added trip view STAB and route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { TripViewComponent } from './views/trip-view/trip-view.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    {path: 'trip/:title', component: TripViewComponent}
 ];
--- client/src/app/views/trip-view/trip-view.component.ts
+++ client/src/app/views/trip-view/trip-view.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-trip-view',
@@ -8,6 +9,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './trip-view.component.html',
   styleUrl: './trip-view.component.scss'
 })
-export class TripViewComponent {
 
+export class TripViewComponent {
+  tripTitle: string = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.tripTitle = this.route.snapshot.params['title'];
+  }
 }
