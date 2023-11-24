66164ddbee13b44f8f1cb6b12b854d90a179fef1
Smichmich <michael.alimi2@gmail.com>
2023-11-24 14:27:52 +0200

    Front: replaced STAB attraction with some basic data. added tripplan with attractions to tip view

--- client/src/app/components/attraction/attraction.component.ts
+++ client/src/app/components/attraction/attraction.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import Attraction from '../../models/attraction.model';
 @Component({
   selector: 'app-attraction',
   standalone: true,
@@ -8,6 +8,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './attraction.component.html',
   styleUrl: './attraction.component.scss'
 })
-export class AttractionComponent {
 
+export class AttractionComponent {
+  @Input("attraction") attraction?: Attraction;
+  editMode: boolean = false;
+
+  onToggleEdit() {
+    this.editMode = true;
+  }
+
+  onCancel() {
+    this.editMode = false;
+    // discard changes
+  }
+
+  onSave() {
+    this.editMode = false;
+    // save changes
+  }
 }
--- client/src/app/views/trip-view/trip-view.component.ts
+++ client/src/app/views/trip-view/trip-view.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { TripPlan } from '../../models/TripPlan';
+import Attraction from '../../models/attraction.model';
+import { AttractionComponent } from '../../components/attraction/attraction.component';
 
 @Component({
   selector: 'app-trip-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AttractionComponent],
   templateUrl: './trip-view.component.html',
   styleUrl: './trip-view.component.scss'
 })
 
 export class TripViewComponent {
   tripTitle: string = '';
+  tripPlan: TripPlan = new TripPlan(
+    '1',
+    'The big trip1',
+    [ new Attraction(
+      '1', "attraction 1",
+      {lat: 1, lng: 2}, false,
+      {startTime: "11:00", endTime: "12:00"},
+      {startDate: new Date(), endDate: new Date()},
+      "this is an attraction",
+      900)],
+    ['Michal', 'Noa'],
+    {startDate: new Date(), endDate: new Date()},
+    ""
+  );
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.tripTitle = this.route.snapshot.params['title'];
+    // load trip from trip services
   }
 }
