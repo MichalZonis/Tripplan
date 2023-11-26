b6cff329016fb0b9226ac14a90dee415190467f2
Smichmich <michael.alimi2@gmail.com>
2023-11-26 14:43:10 +0200

    Front: added edit attraction with save (no backend access for now)

--- client/src/app/components/attraction/attraction.component.ts
+++ client/src/app/components/attraction/attraction.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import Attraction from '../../models/attraction.model';
+import { FormsModule } from '@angular/forms';
+import { DateValueAccessorModule } from 'angular-date-value-accessor';
+
 @Component({
   selector: 'app-attraction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, DateValueAccessorModule],
   templateUrl: './attraction.component.html',
   styleUrl: './attraction.component.scss'
 })
@@ -13,6 +16,28 @@ export class AttractionComponent {
   @Input("attraction") attraction?: Attraction;
   editMode: boolean = false;
 
+  newName: string = '';
+  newDescription: string = '';
+  newStartTime: string = '';
+  newEndTime: string = '';
+  newStartDate: Date = new Date();
+  newEndDate: Date = new Date();
+  newAttractionPrice: number = 0;
+
+  ngOnInit() {
+    this.resetFormData()
+  }
+
+  resetFormData() {
+    this.newDescription = this.attraction?.description || ''
+    this.newName = this.attraction?.name || '';
+    this.newStartTime = this.attraction?.visitHours.startTime || '';
+    this.newEndTime = this.attraction?.visitHours.endTime || '';
+    this.newStartDate = this.attraction?.visitDates.startDate || new Date();
+    this.newEndDate = this.attraction?.visitDates.endDate || new Date();
+    this.newAttractionPrice = this.attraction?.attractionPrice || 0;
+  }
+
   onToggleEdit() {
     this.editMode = true;
   }
@@ -20,10 +45,19 @@ export class AttractionComponent {
   onCancel() {
     this.editMode = false;
     // discard changes
+    this.resetFormData();
   }
 
   onSave() {
     this.editMode = false;
     // save changes
+    if (this.attraction) {
+      this.attraction.name = this.newName
+      this.attraction.description = this.newDescription;
+      this.attraction.visitHours.startTime = this.newStartTime;
+      this.attraction.visitHours.endTime = this.newEndTime;
+      this.attraction.visitDates.startDate = this.newStartDate;
+      this.attraction.visitDates.endDate = this.newEndDate;
+    }
   }
 }
