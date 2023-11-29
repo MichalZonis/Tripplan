d0855f57572fb5e4dd7bdf907ba8b9c1613aa799
Michal Zonis <michal2467300@gmail.com>
2023-11-30 01:00:58 +0200

    date range picker with ngbootstrap

--- client/src/app/components/create-trip/create-trip.component.ts
+++ client/src/app/components/create-trip/create-trip.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule, NativeDateAdapter } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { TripPlanService } from '../../services/trip-plan.service';
+
+import { NgbCalendar, NgbDate, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-create-trip',
@@ -12,15 +14,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   imports: [
     CommonModule,
     MatFormFieldModule,
-    MatDatepickerModule,
     FormsModule,
     ReactiveFormsModule,
     MatNativeDateModule,
+    NgbDatepickerModule
   ],
   templateUrl: './create-trip.component.html',
   styleUrl: './create-trip.component.scss',
 })
 export class CreateTripComponent {
+  calendar = inject(NgbCalendar);
 
   title = new FormControl('');
 
@@ -30,10 +33,47 @@ export class CreateTripComponent {
     endDate: new FormControl<Date | null>(null),
   });
 
+  hoveredDate: NgbDate | null = null;
+  fromDate: NgbDate = this.calendar.getToday();
+  toDate: NgbDate | null = this.calendar.getNext(this.fromDate, 'd', 10);
 
   constructor(
-    public dialogRef: MatDialogRef<CreateTripComponent>
+    public dialogRef: MatDialogRef<CreateTripComponent>,
+    public s_tripPlan: TripPlanService
   ) { }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return (
+      this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+    );
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return (
+      date.equals(this.fromDate) ||
+      (this.toDate && date.equals(this.toDate)) ||
+      this.isInside(date) ||
+      this.isHovered(date)
+    );
+  }
+
+
+
 
 
 }
