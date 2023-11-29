2bf848cedb8af11f33d7d5af3941359884239434
Michal Zonis <michal2467300@gmail.com>
2023-11-30 01:01:07 +0200

    comment demo trips

--- client/src/app/views/home/home.component.ts
+++ client/src/app/views/home/home.component.ts
@@ -20,59 +20,59 @@ export class HomeComponent {
   constructor(public s_tripPlan: TripPlanService, public dialog: MatDialog) { }
   trips$ = this.s_tripPlan.tripPlans$;
 
-  trips: TripPlan[] = [{
-    title: "The big trip1",
-    dates: {
-      startDate: new Date(2022, 10, 4),
-      endDate: new Date(2022, 11, 14)
-    },
-    id: "1",
-    planners: ["Michal", "Noa"],
-    thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
-    // startDate: new Date(2023, 9, 15),
-    // endDate: new Date(2024, 1, 15),
-  }, {
-    title: "The big trip2",
-    id: "1",
-    dates: {
-      startDate: new Date(2022, 10, 4),
-      endDate: new Date(2022, 11, 14)
-    },
-    planners: ["Michal"],
-    thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
-    // startDate: new Date(2023, 9, 15),
-    // endDate: new Date(2024, 1, 15),
-  }, {
-    title: "The big trip",
-    id: "1",
-    planners: ["Michal"], dates: {
-      startDate: new Date(2022, 10, 4),
-      endDate: new Date(2022, 11, 14)
-    },
-    thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
-    // startDate: new Date(2023, 9, 15),
-    // endDate: new Date(2024, 1, 15),
-  }, {
-    title: "The big trip",
-    id: "1", dates: {
-      startDate: new Date(2022, 10, 4),
-      endDate: new Date(2022, 11, 14)
-    },
-    planners: ["Michal"],
-    thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
-    // startDate: new Date(2023, 9, 15),
-    // endDate: new Date(2024, 1, 15),
-  }, {
-    title: "The big trip",
-    id: "1",
-    planners: ["Michal"], dates: {
-      startDate: new Date(2022, 10, 4),
-      endDate: new Date(2022, 11, 14)
-    },
-    thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
-    // startDate: new Date(2023, 9, 15),
-    // endDate: new Date(2024, 1, 15),
-  },]
+  // trips: TripPlan[] = [{
+  //   title: "The big trip1",
+  //   dates: {
+  //     startDate: new Date(2022, 10, 4),
+  //     endDate: new Date(2022, 11, 14)
+  //   },
+  //   id: "1",
+  //   planners: ["Michal", "Noa"],
+  //   thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
+  //   // startDate: new Date(2023, 9, 15),
+  //   // endDate: new Date(2024, 1, 15),
+  // }, {
+  //   title: "The big trip2",
+  //   id: "1",
+  //   dates: {
+  //     startDate: new Date(2022, 10, 4),
+  //     endDate: new Date(2022, 11, 14)
+  //   },
+  //   planners: ["Michal"],
+  //   thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
+  //   // startDate: new Date(2023, 9, 15),
+  //   // endDate: new Date(2024, 1, 15),
+  // }, {
+  //   title: "The big trip",
+  //   id: "1",
+  //   planners: ["Michal"], dates: {
+  //     startDate: new Date(2022, 10, 4),
+  //     endDate: new Date(2022, 11, 14)
+  //   },
+  //   thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
+  //   // startDate: new Date(2023, 9, 15),
+  //   // endDate: new Date(2024, 1, 15),
+  // }, {
+  //   title: "The big trip",
+  //   id: "1", dates: {
+  //     startDate: new Date(2022, 10, 4),
+  //     endDate: new Date(2022, 11, 14)
+  //   },
+  //   planners: ["Michal"],
+  //   thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
+  //   // startDate: new Date(2023, 9, 15),
+  //   // endDate: new Date(2024, 1, 15),
+  // }, {
+  //   title: "The big trip",
+  //   _id: "1",
+  //   planners: ["Michal"], dates: {
+  //     startDate: new Date(2022, 10, 4),
+  //     endDate: new Date(2022, 11, 14)
+  //   },
+  //   thumbnail: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
+  //   // startDate: new Date(2023, 9, 15),
+  //   // endDate: new Date(2024, 1, 15),
+  // },]
 
   ngOnInit() {
     this.s_tripPlan.getAllTrips()
