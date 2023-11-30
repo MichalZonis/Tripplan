8d867463c1cbb3f2256d1010339d943a4f286b96
Michal Zonis <michal2467300@gmail.com>
2023-11-30 22:27:07 +0200

    edit trip

--- client/src/app/components/trip-plan-card/trip-plan-card.component.ts
+++ client/src/app/components/trip-plan-card/trip-plan-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import TripPlan from "../../models/TripPlan";
 import { TripPlanService } from '../../services/trip-plan.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -14,10 +15,26 @@ import { TripPlanService } from '../../services/trip-plan.service';
 export class TripPlanCardComponent {
 
   @Input({ required: true }) tripPlan!: TripPlan
+  isEditMode: boolean = false;
+
+  editForm!: FormGroup
 
   constructor(public s_tripPlan: TripPlanService) { }
 
+  ngOnInit() {
+
+  }
   delete() {
     this.s_tripPlan.deleteTrip(this.tripPlan);
+  }
+
+  editTrip() {
+    this.isEditMode = true;
+    this.editForm = new FormGroup({
+      titleCtrl: new FormControl(this.tripPlan.title),
+      startDateCtrl: new FormControl(this.tripPlan.dates.startDate),
+      endDateCtrl: new FormControl(this.tripPlan.dates.endDate)
+    })
+    console.log(this.editForm.controls["titleCtrl"])
   }
 }
