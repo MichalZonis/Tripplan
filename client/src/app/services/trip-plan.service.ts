4f402f0c9827c022107555958c377119df45da1d
Michal Zonis <michal2467300@gmail.com>
2023-11-30 02:44:01 +0200

    add trip with dialog

--- client/src/app/services/trip-plan.service.ts
+++ client/src/app/services/trip-plan.service.ts
@@ -15,16 +15,16 @@ export class TripPlanService {
 
   constructor(private http: HttpService) { }
 
-  async createTripPlan() {
-    const newTrip: TripPlan =
-    {
-      title: "tokyo is the best",
-      dates: {
-        startDate: new Date(2022, 3, 20),
-        endDate: new Date(2022, 4, 10)
-      },
-      planners: ["Yair", "Michal"]
-    }
+  async createTripPlan(newTrip: TripPlan) {
+    // const  =
+    // {
+    //   title: "tokyo is the best",
+    //   dates: {
+    //     startDate: new Date(2022, 3, 20),
+    //     endDate: new Date(2022, 4, 10)
+    //   },
+    //   planners: ["Yair", "Michal"]
+    // }
     this.http.post("tripPlan/create", { newTrip }).subscribe((newTrip: any) => {
       this._tripPlans.next([...this._tripPlans.value, newTrip])
     });
